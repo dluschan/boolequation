86da89f69c96231b6a2d27197ee77e1be8d3af60
Dmitry Luschan <dluschan@gmail.com>
2015-05-15 15:13:39 +0300

    simple solve ready

--- main.cpp
+++ main.cpp
@@ -8,18 +8,28 @@ int main()
     const int n = 9;
     for (unsigned int i = 0; i < pow(2, n); ++i)
     {
-        bool a[n];
+        bool x[n];
         unsigned k = i;
         for (unsigned int j = 0; j < n; ++j)
         {
-            a[n-j-1] = 0x1 & k;
+            x[n-j-1] = 0x1 & k;
             k >>= 1;
         }
-        for (unsigned int j = 0; j < n; ++j)
+        int f = 1;
+        for (unsigned int j = 0; j < n-2; ++j)
         {
-            cout << a[j];
+            if ((x[j] != x[j+1]) && (x[j] != x[j+2]))
+            {
+                f = 0;
+                break;
+            }
         }
-        cout << endl;
+        if (f)
+        {
+            for (unsigned int j = 0; j < n; ++j)
+                cout << x[j];
+            cout << endl;
+        }
     }
     return 0;
 }
