1e90009591bde77c4dc7b6e3706ddbbadd67e209
Dmitry Luschan <dluschan@gmail.com>
2015-06-01 01:54:43 +0300

    formatting

--- main.cpp
+++ main.cpp
@@ -7,30 +7,30 @@ const int n = 9;
 
 bool fun(bool *x)
 {
-    for (unsigned int j = 0; j < n-2; ++j)
-        if ((x[j] != x[j+1]) && (x[j] != x[j+2]))
-            return false;
-    return true;
+	for (unsigned int j = 0; j < n-2; ++j)
+		if ((x[j] != x[j+1]) && (x[j] != x[j+2]))
+			return false;
+	return true;
 }
 
 int main()
 {
-    for (unsigned int i = 0; i < pow(2, n); ++i)
-    {
-        bool x[n];
-        unsigned k = i;
-        for (unsigned int j = 0; j < n; ++j)
-        {
-            x[n-j-1] = 0x1 & k;
-            k >>= 1;
-        }
-        if (fun(x))
-        {
-            for (unsigned int j = 0; j < n; ++j)
-                cout << x[j];
-            cout << endl;
-        }
-    }
-    return 0;
+	for (unsigned int i = 0; i < pow(2, n); ++i)
+	{
+		bool x[n];
+		unsigned k = i;
+		for (unsigned int j = 0; j < n; ++j)
+		{
+			x[n-j-1] = 0x1 & k;
+			k >>= 1;
+		}
+		if (fun(x))
+		{
+			for (unsigned int j = 0; j < n; ++j)
+				cout << x[j];
+			cout << endl;
+		}
+	}
+	return 0;
 }
 
