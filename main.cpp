fc4456f948c82aa08414f6baae7e2955b78bab56
Dmitry Luschan <dluschan@gmail.com>
2015-06-09 09:50:04 +0300

    rename calculate logical expression instead of algebraical expression

--- main.cpp
+++ main.cpp
@@ -18,21 +18,20 @@ class Expr
 {
 public:
 	Expr(){}
-	virtual operator double() const = 0;
+	virtual operator bool() const = 0;
 };
-
 typedef shared_ptr<Expr> PExpr;
 
-class PlusToken: public Expr
+class Disjunction: public Expr
 {
 public:
-	PlusToken(PExpr pLeft, PExpr pRight)
+	Disjunction(PExpr pLeft, PExpr pRight)
 		: left (pLeft )
 		, right(pRight)
 	{}
-	operator double() const
+	operator bool() const
 	{
-		return *left + *right;
+		return *left || *right;
 	}
 
 private:
@@ -40,49 +39,64 @@ private:
 	PExpr right;
 };
 
-class MulToken: public Expr
+class Conjunction: public Expr
 {
 public:
-	MulToken(PExpr pLeft, PExpr pRight)
+	Conjunction(PExpr pLeft, PExpr pRight)
 		: left (pLeft )
 		, right(pRight)
 	{}
-	operator double() const
+	operator bool() const
 	{
-		return *left * *right;
+		return *left && *right;
 	}
 
 private:
 	PExpr left;
 	PExpr right;
+};
+
+class Negation: public Expr
+{
+public:
+	Negation(PExpr pExpr)
+		: expr(pExpr)
+	{}
+	operator bool() const
+	{
+		return !(*expr);
+	}
+
+private:
+	PExpr expr;
 };
 
 class ValueToken: public Expr
 {
 public:
-	ValueToken(double v)
-		: value(v)
+	ValueToken(bool b)
+		: value(b)
 	{}
-	operator double() const
+	operator bool() const
 	{
 		return value;
 	}
 
 private:
-	double value;
+	bool value;
 };
 
 enum Token_value
 {
-	NAME, NUMBER, END,
-	PLUS = '+', MINUS = '-', MUL = '*', DIV = '/',
-	PRINT = ';', ASSIGN = '=', LP = '(', RP = ')'
+	NAME, NUMBER, END = ';',
+	OR = '|', AND = '&', NOT = '!',
+	EQV = '=', LP = '(', RP = ')'
 };
 
-Token_value curr_tok = PRINT;
-double number_value;
+Token_value curr_tok = END;
+bool number_value;
 string string_value;
-map<string, double> table;
+map<string, bool> table;
 int no_of_errors;
 
 PExpr expr(bool);
@@ -103,13 +117,12 @@ Token_value get_token()
 	case 0:
 		return curr_tok = END;
 	case ';':
-	case '*':
-	case '/':
-	case '+':
-	case '-':
+	case '&':
+	case '|':
 	case '(':
 	case ')':
 	case '=':
+	case '!':
 		return curr_tok = Token_value(ch);
 	case '0': case '1': case '2': case '3': case '4':
 	case '5': case '6': case '7': case '8': case '9':
@@ -124,8 +137,11 @@ Token_value get_token()
 			cin >> string_value;
 			return curr_tok = NAME;
 		}
-		error("неправильная лексема");
-		return curr_tok = PRINT;
+		else
+		{
+			error("неправильная лексема");
+			return curr_tok = END;
+		}
 	}
 }
 
@@ -138,7 +154,7 @@ PExpr prim(bool get)
 	{
 	case NUMBER:
 	{
-		double v = number_value;
+		bool v = number_value;
 		get_token();
 		return make_shared<ValueToken>(v);
 	}
@@ -162,10 +178,10 @@ PExpr term(bool get)
 	{
 		switch(curr_tok)
 		{
-		case MUL:
+		case AND:
 		{
 			PExpr pRight = prim(true);
-			pLeft = std::make_shared<MulToken>(pLeft, pRight);
+			pLeft = make_shared<Conjunction>(pLeft, pRight);
 			break;
 		}
 		default:
@@ -177,14 +193,14 @@ PExpr term(bool get)
 PExpr expr(bool get)
 {
 	PExpr pLeft = term(get);
-	while(true)
+	while (true)
 	{
 		switch(curr_tok)
 		{
-		case PLUS:
+		case OR:
 		{
 			PExpr pRight = term(true);
-			pLeft = make_shared<PlusToken>(pLeft, pRight);
+			pLeft = make_shared<Disjunction>(pLeft, pRight);
 			break;
 		}
 		default:
@@ -201,8 +217,6 @@ void compile()
 		get_token();
 		if (curr_tok == END)
 			break;
-		if (curr_tok == PRINT)
-			continue;
 		cout << *expr(false) << endl;
 	}
 }
@@ -244,7 +258,7 @@ int main(int argc, char* argv[])
 		}
 		if (!strcmp(argv[1],"-i"))
 		{
-			cout << "интерактивный режим еще не поддерживается, пока запускается просто калькулятор" << endl;
+			cout << "интерактивный режим работы" << endl;
 			compile();
 			return(10);
 		}
